2a78c669bafda92194cbb0ebec619c317d192859
Adam Mackey <adambmackey@gmail.com>
2022-04-06 15:25:03 -0400

    trial

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const PORT = process.env.PORT || 4006
+const PORT = process.env.PORT || 4005
 
 app.listen(PORT, () => { console.log(`listening on ${PORT}`)})
